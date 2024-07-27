5b3c1bbe404648c0ffb60aed7e79fd1e29b52398
“Henil <henilgajjar08@gmail.com>
2024-07-27 01:01:28 -0400

    Commit

---
+++ app.py
@@ -0,0 +1,137 @@
+import subprocess
+
+args = [
+    "sudo apt-get install poppler-utils",
+    "sudo apt-get install libleptonica-dev tesseract-ocr libtesseract-dev python3-pil tesseract-ocr-eng tesseract-ocr-script-latn"
+    "pip install unstructured-pytesseract",
+    "pip install tesseract-ocr"
+]
+for arg in args:
+    st.info(f"Running '{' '.join(arg)}'")
+    result = subprocess.run(arg, capture_output=True, text=True)
+
+
+import io
+import re
+import os
+import uuid
+import utils
+import base64
+import openai
+from PIL import Image
+from typing import Any
+import streamlit as st
+from pydantic import BaseModel
+from IPython.display import HTML, display
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryStore
+from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+from unstructured.partition.pdf import partition_pdf
+
+fpath = "Data/Extracted Data"
+
+# Title
+st.title("Multimodal RAG PDF Q&A using GPT-4o")
+st.write("Upload Pdf's and chat with their content")
+
+## Sidebar for settings
+st.sidebar.title("Settings")
+apikey = st.sidebar.text_input("Enter your Open AI API Key:", type="password")
+
+## Upload PDF
+uploaded_file=st.file_uploader("Choose A Pdf file", type="pdf", accept_multiple_files=False)
+
+## Process uploaded  PDF's
+if uploaded_file and apikey:
+    raw_pdf_elements=partition_pdf(
+        filename=uploaded_file,                  # mandatory
+        strategy="hi_res",                                 # mandatory to use ``hi_res`` strategy
+        extract_images_in_pdf=True,                       # mandatory to set as ``True``
+        extract_image_block_types=["Image", "Table"],          # optional
+        extract_image_block_to_payload=False,                  # optional
+        extract_image_block_output_dir=fpath,  # optional - only works when ``extract_image_block_to_payload=False``
+    )
+
+    text=[]
+    img=[]
+    table=[]
+
+    for element in raw_pdf_elements:
+        if "unstructured.documents.elements.Header" in str(type(element)):
+                    text.append(str(element))
+        elif "unstructured.documents.elements.Image" in str(type(element)):
+                    img.append(str(element))
+        elif "unstructured.documents.elements.Table" in str(type(element)):
+                    table.append(str(element))
+        elif "unstructured.documents.elements.Footer" in str(type(element)):
+                    text.append(str(element))
+        elif "unstructured.documents.elements.Title" in str(type(element)):
+                    text.append(str(element))
+        elif "unstructured.documents.elements.NarrativeText" in str(type(element)):
+                    text.append(str(element))
+        elif "unstructured.documents.elements.Text" in str(type(element)):
+                    text.append(str(element))
+        elif "unstructured.documents.elements.ListItem" in str(type(element)):
+                    text.append(str(element))
+    
+    # Table Summaries
+    prompt_text = """You are an assistant tasked with summarizing tables for retrieval. \
+        These summaries will be embedded and used to retrieve the raw table elements. \
+        Give a concise summary of the table that is well optimized for retrieval. Table:{element} """
+    prompt_table = ChatPromptTemplate.from_template(prompt_text)
+    table_summaries = utils.text_table_summarizer(table, apikey, prompt_table)
+
+    # Text Summaries
+    prompt_text = """You are an assistant tasked with summarizing text for retrieval. \
+        These summaries will be embedded and used to retrieve the raw text elements. \
+        Give a concise summary of the table or text that is well optimized for retrieval.text: {element} """
+    prompt_for_text = ChatPromptTemplate.from_template(prompt_text)
+    text_summaries = utils.text_table_summarizer(text, apikey, prompt_for_text)
+
+    # Image Summaries
+    img_base64_list, image_summaries = utils.generate_img_summaries(fpath)
+
+    # Mutlivector Retreiver
+    vectorstore = Chroma(
+        collection_name="mm_rag", embedding_function=OpenAIEmbeddings()
+    )
+    retriever_multi_vector_img = utils.create_multi_vector_retriever(
+        vectorstore,
+        text_summaries,
+        text,
+        table_summaries,
+        table,
+        image_summaries,
+        img_base64_list,
+    )
+
+    # RAG chain
+    chain_multimodal_rag = utils.multi_modal_rag_chain(retriever_multi_vector_img)
+
+    user_input = st.text_input("Your question:")
+    if user_input:
+        docs = retriever_multi_vector_img.invoke(user_input)
+        response = chain_multimodal_rag.invoke(user_input)
+
+        st.write("Assistant:", response['answer'])
+
+        for doc in docs:
+            if doc in image_summaries:
+                index = image_summaries.index(doc)
+                st.write("Image Referred from:", utils.plt_img_base64(img_base64_list[index]))
+            elif doc in text_summaries:
+                st.write("Text Referred from:", doc)
+            elif doc in table_summaries:
+                st.write("Table Reffered from:", doc)
+
+elif uploaded_file and not apikey:
+    st.info("Enter API Key to proceed!")
+else:
+    st.info("Upload the Document")
---
+++ utils.py
@@ -0,0 +1,250 @@
+import os
+import io
+import re
+import uuid
+import openai
+import base64
+from PIL import Image
+from IPython.display import HTML, display
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from langchain.retrievers.multi_vector import MultiVectorRetriever
+from langchain.storage import InMemoryStore
+from langchain_community.vectorstores import Chroma
+from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
+
+
+def text_table_summarizer(docs, apikey, prompt):
+    # Text summary chain
+    openai.api_key = apikey
+    model = ChatOpenAI(temperature=0, model="gpt-4")
+    summarize_chain = {"element": lambda x: x} | prompt | model | StrOutputParser()
+    summaries = summarize_chain.batch(docs, {"max_concurrency": 5})
+
+    return summaries
+
+def encode_image(image_path):
+    """Getting the base64 string"""
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+    
+def image_summarize(img_base64, prompt):
+    """Make image summary"""
+    chat = ChatOpenAI(model="gpt-4o", max_tokens=1024)
+
+    msg = chat.invoke(
+        [
+            HumanMessage(
+                content=[
+                    {"type": "text", "text": prompt},
+                    {
+                        "type": "image_url",
+                        "image_url": {"url": f"data:image/jpeg;base64,{img_base64}"},
+                    },
+                ]
+            )
+        ]
+    )
+    return msg.content
+
+def generate_img_summaries(path):
+    """
+    Generate summaries and base64 encoded strings for images
+    path: Path to list of .jpg files extracted by Unstructured
+    """
+
+    # Store base64 encoded images
+    img_base64_list = []
+
+    # Store image summaries
+    image_summaries = []
+
+    # Prompt
+    prompt = """You are an assistant tasked with summarizing images for retrieval. \
+    These summaries will be embedded and used to retrieve the raw image. \
+    Give a concise summary of the image that is well optimized for retrieval."""
+
+    # Apply to images
+    for img_file in sorted(os.listdir(path)):
+        if img_file.endswith(".jpg") and not img_file.startswith("table"):
+            img_path = os.path.join(path, img_file)
+            base64_image = encode_image(img_path)
+            img_base64_list.append(base64_image)
+            image_summaries.append(image_summarize(base64_image, prompt))
+
+
+    return img_base64_list, image_summaries
+    
+
+def create_multi_vector_retriever(vectorstore, text_summaries, texts, table_summaries, table, image_summaries, img):
+    """
+    Create retriever that indexes summaries, but returns raw images or texts
+    """
+
+    # Initialize the storage layer
+    store = InMemoryStore()
+    id_key = "doc_id"
+
+    # Create the multi-vector retriever
+    retriever = MultiVectorRetriever(
+        vectorstore=vectorstore,
+        docstore=store,
+        id_key=id_key,
+    )
+
+
+    # Helper function to add documents to the vectorstore and docstore
+    def add_documents(retriever, doc_summaries, doc_contents):
+
+      doc_ids = [str(uuid.uuid4()) for _ in doc_contents]
+
+      summary_docs = [
+            Document(page_content=s, metadata={id_key: doc_ids[i]}) for i, s in enumerate(doc_summaries)
+          ]
+
+      retriever.vectorstore.add_documents(summary_docs)
+      retriever.docstore.mset(list(zip(doc_ids, doc_contents)))
+
+    # Add texts, tables, and images
+    # Check that text_summaries is not empty before adding
+    if text_summaries:
+        add_documents(retriever, text_summaries, texts)
+    # Check that table_summaries is not empty before adding
+    if table_summaries:
+        add_documents(retriever, table_summaries, table)
+    # Check that image_summaries is not empty before adding
+    if image_summaries:
+        add_documents(retriever, image_summaries, img)
+
+    return retriever
+
+
+def plt_img_base64(img_base64):
+    """Disply base64 encoded string as image"""
+    # Create an HTML img tag with the base64 string as the source
+    image_html = f'<img src="data:image/jpeg;base64,{img_base64}" />'
+    # Display the image by rendering the HTML
+    display(HTML(image_html))
+
+
+def looks_like_base64(sb):
+    """Check if the string looks like base64"""
+    return re.match("^[A-Za-z0-9+/]+[=]{0,2}$", sb) is not None
+
+
+def is_image_data(b64data):
+    """
+    Check if the base64 data is an image by looking at the start of the data
+    """
+    image_signatures = {
+        b"\xFF\xD8\xFF": "jpg",
+        b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A": "png",
+        b"\x47\x49\x46\x38": "gif",
+        b"\x52\x49\x46\x46": "webp",
+    }
+    try:
+        header = base64.b64decode(b64data)[:8]  # Decode and get the first 8 bytes
+        for sig, format in image_signatures.items():
+            if header.startswith(sig):
+                return True
+        return False
+    except Exception:
+        return False
+    
+
+def resize_base64_image(base64_string, size=(128, 128)):
+    """
+    Resize an image encoded as a Base64 string
+    """
+    # Decode the Base64 string
+    img_data = base64.b64decode(base64_string)
+    img = Image.open(io.BytesIO(img_data))
+
+    # Resize the image
+    resized_img = img.resize(size, Image.LANCZOS)
+
+    # Save the resized image to a bytes buffer
+    buffered = io.BytesIO()
+    resized_img.save(buffered, format=img.format)
+
+    # Encode the resized image to Base64
+    return base64.b64encode(buffered.getvalue()).decode("utf-8")
+
+
+
+def split_image_text_types(docs):
+    """
+    Split base64-encoded images and texts
+    """
+    b64_images = []
+    texts = []
+
+    for doc in docs:
+        # Check if the document is of type Document and extract page_content if so
+        if isinstance(doc, Document):
+            doc = doc.page_content
+        if looks_like_base64(doc) and is_image_data(doc):
+            doc = resize_base64_image(doc, size=(1300, 600))
+            b64_images.append(doc)
+        else:
+            texts.append(doc)
+
+    return {"images": b64_images, "texts": texts}
+
+
+def img_prompt_func(data_dict):
+    """
+    Join the context into a single string
+    """
+    #print(data_dict)
+    formatted_texts = "\n".join(data_dict["context"]["texts"])
+    messages = []
+
+    # Adding image(s) to the messages if present
+    if data_dict["context"]["images"]:
+        for image in data_dict["context"]["images"]:
+            image_message = {
+                "type": "image_url",
+                "image_url": {"url": f"data:image/jpeg;base64,{image}"},
+            }
+            messages.append(image_message)
+
+    # Adding the text for analysis
+    text_message = {
+        "type": "text",
+        "text": (
+            "You are a helpful assistant.\n"
+            "You will be given a mixed info(s) .\n"
+            "Use this information to provide relevant information to the user question. \n"
+            f"User-provided question: {data_dict['question']}\n\n"
+            "Text and / or tables:\n"
+            f"{formatted_texts}"
+        ),
+    }
+    messages.append(text_message)
+    return [HumanMessage(content=messages)]
+
+
+def multi_modal_rag_chain(retriever):
+    """
+    Multi-modal RAG chain
+    """
+
+    # Multi-modal LLM
+    model = ChatOpenAI(temperature=0, model="gpt-4o", max_tokens=1024)
+
+
+    # RAG pipeline
+    chain = (
+        {
+            "context": retriever | RunnableLambda(split_image_text_types),
+            "question": RunnablePassthrough(),
+        }
+        | RunnableLambda(img_prompt_func)
+        | model
+        | StrOutputParser()
+    )
+
+    return chain
