64e4b26f83dc20228425631e3524755a999a0996
“Henil <henilgajjar08@gmail.com>
2024-07-28 01:03:37 -0400

    Chroma

--- app.py
+++ app.py
@@ -24,8 +24,14 @@ from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
 from unstructured.partition.pdf import partition_pdf
 
+## Chroma Dependencies
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 fpath = "Data/Extracted Data"
 
+# Clear the folder
 try:
     shutil.rmtree(fpath)
 except:
